82c18d3ee8fb8931c02a4155052b80f342df283e
James Campbell <idevdroidapps@gmail.com>
2020-07-22 08:05:55 -0400

    initial commit

---
+++ app/src/main/java/com/shieldai/samples/shieldaichallenge/util/AppExecutors.kt
@@ -0,0 +1,37 @@
+package com.shieldai.samples.shieldaichallenge.util
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+/**
+ * Global executor pools for the whole application.
+ *
+ * Grouping tasks like this avoids the effects of task starvation (e.g. disk reads don't wait behind
+ * webservice requests).
+ *
+ * For unit testing, note that we can extend this class and inject our own executors for deterministic
+ * behavior
+ *
+ * This is based on the same AppExecutors class defined in the Android architecture repo
+ */
+
+const val THREAD_COUNT = 3
+
+class AppExecutors constructor(
+    val diskIO: Executor = Executors.newSingleThreadExecutor(),
+    val networkIO: ExecutorService = Executors.newFixedThreadPool(THREAD_COUNT),
+    val backgroundThreadPool: ExecutorService = Executors.newFixedThreadPool(THREAD_COUNT),
+    val mainThread: Executor = MainThreadExecutor()
+) {
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+}
